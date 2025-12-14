fb724d71cc1ad8c5786f62670c0afa3c6add80de
Mr-Rizen <kilianleroux56@gmail.com>
2025-12-14 14:32:53 +0100

    FIX : variables.tf et Dockerfile.jenkins

--- infra/k3s/variables.tf
+++ infra/k3s/variables.tf
@@ -1,17 +1,19 @@
 variable "cluster_name" {
-  default = "tf-devops-lab"
+    default = "tf-devops-lab"
 }
 
 variable "app_replicas" {
-  default = 3
+    default = 3
 }
 
 variable "app_label" {
-  default = "flask-web"
+    default = "flask-web"
 }
 
 # Nouvelle variable pour l'image construite par Jenkins
 variable "app_image_tag" {
-  description = "Le tag de l'image Docker construit par Jenkins (ex: kikih/devops-webapp:1)"
-  type        = string
+    description = "Le tag de l'image Docker construit par Jenkins (ex: kikih/devops-webapp:1)"
+    type        = string
+    # CORRECTION : Ajout de la valeur par défaut pour permettre à 'terraform destroy' de s'exécuter sans erreur.
+    default     = ""
 }
