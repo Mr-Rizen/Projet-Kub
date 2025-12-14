a12b86cc96ef1d90a8ea8d4b894d25d1852789bf
Mr-Rizen <kilianleroux56@gmail.com>
2025-12-14 11:16:43 +0100

    Delete Projet-Kub directory

--- Projet-Kub/infra/k3s/variables.tf
+++
@@ -1,6 +0,0 @@
-# 3. Configuration du Provider Kubernetes
-# Il va automatiquement chercher le kubeconfig dans les chemins par défaut
-provider "kubernetes" {
-  # Pour K3d, l'authentification est gérée par le fichier kubeconfig créé en 1.4
-  # Terraform utilise ce fichier pour s'authentifier auprès du cluster K3s.
-}
